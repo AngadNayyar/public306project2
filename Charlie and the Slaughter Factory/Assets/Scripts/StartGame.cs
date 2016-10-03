ffe6b5b4ddb9c811be48d5f0a7adf091e606e911
hjadenz <hsam938@aucklanduni.ac.nz>
2016-10-03 23:37:47 +1300

    Create the Username entering page

--- Charlie and the Slaughter Factory/Assets/Scripts/StartGame.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/StartGame.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
-public class StartGame : MonoBehaviour {	
-	public void start () {
+public class StartGame : MonoBehaviour {
+
+	private object username;
+
+	public void toUsername() {
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Username");
+	}
+
+	public void submitAndStart(object usernameInput) {
+		username = usernameInput;
 		UnityEngine.SceneManagement.SceneManager.LoadScene("introlvl1");
 	}
 }
