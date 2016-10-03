b6992ad736740d3aeb90b5e4d443163fc92efe49
hjadenz <hsam938@aucklanduni.ac.nz>
2016-10-04 07:32:50 +1300

    Record entered username

--- Charlie and the Slaughter Factory/Assets/Scripts/StartGame.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/StartGame.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour {
 
-	private object username;
+	private string username;
 
 	public void toUsername() {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Username");
 	}
 
-	public void submitAndStart(object usernameInput) {
-		username = usernameInput;
+	public void submitAndStart(GameObject usernameInput) {
+		username = usernameInput.GetComponentInChildren<Text>().text;
 		UnityEngine.SceneManagement.SceneManager.LoadScene("introlvl1");
 	}
 }
