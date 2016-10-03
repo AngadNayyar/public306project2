bf427800d4b8aa5c626cad774edf3a0c494c5283
harryjacko <hjac660@aucklanduni.ac.nz>
2016-10-03 17:57:26 +1300

    Initial commit, tutorial levels

---
+++ Charlie and the Slaughter Factory/Assets/SceneScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour {
+
+
+
+    void OnTriggerEnter2D() {
+        SceneManager.LoadScene("introlvl2");
+    }
+}
---
+++ Charlie and the Slaughter Factory/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    [HideInInspector]
+    public bool facingRight = true;
+    [HideInInspector]
+    public bool jump = true;
+
+    public float maxSpeed = 3f;
+    public float speed = 20f;
+    public float jumpPower = 150f;
+
+    public bool grounded;
+
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+            rb2d.AddForce(new Vector2(0, 6), ForceMode2D.Impulse);
+
+    }
+
+    void FixedUpdate() {
+
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(Vector2.right * h * speed); // Can move left and right
+
+        if (rb2d.velocity.x > maxSpeed)
+            rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y); // Stops velocity going above max speed
+        if (rb2d.velocity.x < -maxSpeed)
+            rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y); // Stops velocity going above max speed
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+    }
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
