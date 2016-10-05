cae0c2fca81524f7d107a3711ae07f14d42672f4
idaknow <ides542@aucklanduni.ac.nz>
2016-10-05 14:17:35 +1300

    The dog moves left and right, and turns around.

--- Charlie and the Slaughter Factory/Assets/Scripts/EnemyScript.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/EnemyScript.cs
@@ -1,76 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyScript : MonoBehaviour { // Moving Object
+public class EnemyScript : MonoBehaviour
+{
 
-    public int playerDamage;
-    private Animator anim;
-    private Transform target;
-    private bool skipMove;
-
-    public float velocity = 1f;
-    public Transform sightStart;
-    public Transform sightEnd;
-
-    public bool colliding;
-    public LayerMask detectWhat;
+    [HideInInspector]
     public bool facingRight = true;
-    
 
-    // Use this for initialization
-    void Start () {
-        anim = GetComponent<Animator>();
-        //     target = GameObject.FindGameObjectsWithTag("Player").transform;
-        //   base.Start();
-    }
+    private Rigidbody2D rb2d;
+    private Animator anim;
 
-    public float rightLimit = 20f;
-    public float leftLimit = -4.3f;
+    public float rightLimit = 1.0f;
+    public float leftLimit = -4.0f;
     public float speed = 1.0f;
     private int direction = 1;
 
-    void Update()
+    // Use this for initialization
+    void Start()
     {
-        
-        if (transform.position.x > rightLimit)
-        {
-            // facingRight = true;
-            print("right");
-            print(transform.position.x);
-            Flip();
-        }
-        else if (transform.position.x < leftLimit)
-        {
-            //facingRight = false;
-            print("left");
-            print(transform.position.x);
-            print(leftLimit);
-            Flip();
-        }
-      //  Vector3 movement = 
-       // transform.Translate(movement);
     }
 
     // Update is called once per frame
-    /* void Update() {
-         GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, GetComponent<Rigidbody2D>().velocity.y);
-
-         colliding = Physics2D.Linecast(transform.position, transform.position, detectWhat);
-
-         if (colliding)
-         {
-             Flip();
-         }
-     }*/
-
-    void Flip()
+    void Update()
     {
-        facingRight = !facingRight;
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-        colliding = false;
+        
     }
 
-    // void AttemptMove<T> (int xDir, int yDir)
+    void FixedUpdate()
+    {
+        Vector3 movement;
+        if (transform.position.x > rightLimit)
+        {
+            direction = -1;         
+            movement = Vector3.right * speed * Time.deltaTime;
+            transform.Translate(movement);
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (transform.position.x < leftLimit)
+        {
+           direction = 1;
+           movement = Vector3.right * speed * Time.deltaTime;
+           transform.Translate(movement);
+           transform.localRotation = Quaternion.Euler(0, 0, 0); // flips it
+        }
+        else {
+            movement = Vector3.right * speed * Time.deltaTime;
+            transform.Translate(movement);
+        }
+        
+    }
 }
