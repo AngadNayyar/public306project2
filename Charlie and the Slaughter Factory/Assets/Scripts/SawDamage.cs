86bee068ce2d5c438d8780f4ae49c43697a45a74
Angad Nayyar <angadnayyar@hotmail.com>
2016-10-05 16:45:03 +1300

    Health now updates
    Health bar now updates with damage. Nothing happens at 0 damage, and no
    knock back effect yet.

--- Charlie and the Slaughter Factory/Assets/Scripts/SawDamage.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/SawDamage.cs
@@ -6,15 +6,18 @@ public class SawDamage : MonoBehaviour {
     public int attackDamage = 40;  //Damage taken due to hit
 
     PlayerHealth playerHealth;  // Reference to the Charlie's health.
+    GameObject player;  // Reference to Charlie
 
-    void Start() {
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
     }
 
     void Update() {
     }
 
     void OnTriggerEnter2D(Collider2D other){
-        if(other.name == "Charlie"){
+        if(other.CompareTag("Player")){
             playerHealth.TakeDamage(attackDamage);
         }
     }
