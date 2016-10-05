a831eb651aa81a4373cf78154f2b4df42ffdee56
Angad Nayyar <angadnayyar@hotmail.com>
2016-10-05 16:04:08 +1300

    Added Assets and Scripts for PlayerHealth
    Assets and Scripts for PlayerHealth are created, but not yet linked up.

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/SawDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SawDamage : MonoBehaviour {
+    public int attackDamage = 40;  //Damage taken due to hit
+
+    PlayerHealth playerHealth;  // Reference to the Charlie's health.
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.name == "Charlie"){
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
