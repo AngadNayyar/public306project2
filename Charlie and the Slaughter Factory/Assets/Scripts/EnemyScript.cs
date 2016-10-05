7283ad6671b5a310413d8724552c64567c5b6ff1
idaknow <ides542@aucklanduni.ac.nz>
2016-10-05 16:34:32 +1300

    Dog runs faster if the chicken is in the limits

--- Charlie and the Slaughter Factory/Assets/Scripts/EnemyScript.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/EnemyScript.cs
@@ -8,15 +8,35 @@ public class EnemyScript : MonoBehaviour
     public float rightLimit = 1.0f;
     public float leftLimit = -4.0f;
     public float speed = 1.0f;
+    public GameObject player;
 
     // Use this for initialization
-    void Start(){}
+    void Start(){
+        
+    }
 
     // Update is called once per frame
     void Update(){}
 
     void FixedUpdate()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        float player_pos = player.transform.position.x;
+        if (player_pos < rightLimit && player_pos > leftLimit)
+        {
+            if (true) {
+                speed = 5.0f;
+            }
+            else
+            {
+                speed = 1.0f;
+            }
+        }
+        else
+        {
+            speed = 1.0f;
+        }
+
         Vector3 movement = Vector3.right * speed * Time.deltaTime; // makes the speed/ movement of the dog
         transform.Translate(movement); // applies the movement to the dog
 
