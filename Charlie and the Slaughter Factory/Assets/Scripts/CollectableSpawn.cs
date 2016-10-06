39e6f6ed227a4d5942fd773549762bc2ce1798ff
idaknow <ides542@aucklanduni.ac.nz>
2016-10-06 17:03:40 +1300

    Added PickupCollectable and CollectableSpawn Scripts
    britwalker committed this

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/CollectableSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableSpawn : MonoBehaviour
+{
+
+    public Transform[] collectibleSpawns;
+    public GameObject collectible;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < collectibleSpawns.Length; i++)
+        {
+            int collectibleFlip = Random.Range(0, 2);
+            if (collectibleFlip > 0)
+                Instantiate(collectible, collectibleSpawns[i].position, Quaternion.identity);
+        }
+    }
+}
--- Charlie and the Slaughter Factory/Assets/Scripts/PickupCollectable.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/PickupCollectable.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickupCollectable : MonoBehaviour {
+public class PickupCollectible : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Destroy(gameObject);
+    }
 }
