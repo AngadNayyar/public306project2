81a3dbccaec3bbc0d9fc27237c6bf510d9ec20ba
hjadenz <hsam938@aucklanduni.ac.nz>
2016-10-06 22:02:36 +1300

    Add all new cut scenes

--- Charlie and the Slaughter Factory/Assets/Scripts/SwitchSceneScript.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/SwitchSceneScript.cs
@@ -11,4 +11,11 @@ public class SwitchSceneScript : MonoBehaviour {
     	PlayerPrefs.SetString("CurrentScene", newScene);
         SceneManager.LoadScene(PlayerPrefs.GetString(newScene));
     }
+
+    public void OnClick() {
+    	int currentScene = int.Parse(PlayerPrefs.GetString("CurrentScene"));
+    	string newScene = (currentScene + 1).ToString();
+    	PlayerPrefs.SetString("CurrentScene", newScene);
+        SceneManager.LoadScene(PlayerPrefs.GetString(newScene));
+    }
 }
