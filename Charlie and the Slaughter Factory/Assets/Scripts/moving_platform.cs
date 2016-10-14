e8fca5ed23253643de5978fc97240de03716ac28
idaknow <ides542@aucklanduni.ac.nz>
2016-10-15 12:31:13 +1300

    Added moving platform

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/moving_platform.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving_platform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, transform.position.z);
+    }
+}
