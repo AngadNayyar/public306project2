20a2a43e64f75c4f64775f11069ee28916cc1c2d
idaknow <ides542@aucklanduni.ac.nz>
2016-10-15 14:51:58 +1300

    platform movement continued

--- Charlie and the Slaughter Factory/Assets/Scripts/moving_platform.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/moving_platform.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class moving_platform : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    Vector3 Pos1 = new Vector3(-4, 0, 0);
+    Vector3 Pos2 = new Vector3(4, 0, 0);
+    float speed = 1.0f;
+
+    // Use this for initialization
+    void Start () {}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = new Vector3(Mathf.PingPong(Time.time, 3), transform.position.y, transform.position.z);
+        //transform.position = new Vector2(Mathf.PingPong(Time.time, 3), transform.position.y);
+        double fraction = (Mathf.Sin(Time.time * speed) + 1.0) / 2.0;
+        transform.position = Vector3.Lerp(Pos1, Pos2, (float)fraction);
     }
 }
