eb13cfbd763a4abd80fcb82247f8a2f7a881002f
idaknow <ides542@aucklanduni.ac.nz>
2016-10-16 11:34:08 +1300

    Added a claw that follows the player (in progress) & fixed errors

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/Claw_follow_player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Claw_follow_player : MonoBehaviour {
+
+    public Transform target;
+    public float speed = 2.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        transform.position -= transform.right * speed * Time.deltaTime;
+	}
+}
