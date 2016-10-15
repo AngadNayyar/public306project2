16860e2e943699555a886b7a89aee300854d0691
idaknow <ides542@aucklanduni.ac.nz>
2016-10-16 09:49:49 +1300

    Made the mincer doors - open and close

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/Mincer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mincer : MonoBehaviour
+{
+    //speed at which the object moves back and forth
+    public float speed = 3;
+    //direction in which the object moves. 1 is for right and -1 for left
+    public float dir = 1;
+
+    void Update()
+    {
+        //move the object at a rate of 1*speed unit/s along the x-axis
+        transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * dir * speed);
+    }
+
+    //check if the object hits the other two cubes
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //check if the object enter into a trigger with objects named Mincer
+        if (coll.gameObject.tag == "Mincer")
+        {
+            //reverse the current direction 
+            dir *= -1;
+        }
+    }
+
+}
