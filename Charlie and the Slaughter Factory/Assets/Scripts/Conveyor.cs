1d8cf56c347a678bfda14e936130bb2f803016ab
Christina <cbel296@aucklanduni.ac.nz>
2016-10-16 15:34:08 +1300

    commented script to add description for use

--- Charlie and the Slaughter Factory/Assets/Scripts/Conveyor.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/Conveyor.cs
@@ -1,6 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+/*
+ * This script controls conveyor belt mechanics. 
+ * When charlie touches the conveyor, his speed increases.  
+ * 
+ * To use this script, attach it to the conveyor belt platform object, which will need a collision box.  
+ *  
+ * Charlie and the Slaughter factory - Teven Studios
+ */
+
 public class Conveyor : MonoBehaviour {
 
 	// set the speed for the conveyor
@@ -11,15 +21,11 @@ public class Conveyor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// Get the player rigid body
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerBody = player.GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-			
-	}
-
 	// While charlie is on the conveyor add a speed to the right 
 	void OnCollisionStay2D(Collision2D other){
 		if (other.gameObject.CompareTag("Player")){
