d94f38c2d72f2c660f4bd9dac06eb12ce739d255
brittanywalker <brittwalker@outlook.com>
2016-10-16 20:53:00 +1300

    Script for Saw audio added
    Currently replays repeatedly as long as player is within trigger
    distance

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/SawAudio.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class uses a collider so that when Charlie walks within a certain distance of the saw,
+//the audio for the saw will start.
+
+public class SawAudio : MonoBehaviour {
+
+	GameObject player;  // Reference to Charlie
+	public AudioClip sawSound; //This is the variable for the .wav file.
+
+	private AudioSource source; 
+	Rigidbody2D playerBody;
+	Rigidbody2D sawBody;
+
+	private int triggerDistance = 6;
+
+	void Awake () {
+
+		player = GameObject.FindGameObjectWithTag("Player"); //Sets Charlie to the player variable
+		source = GetComponent<AudioSource> (); //Sets the saw audio source to the source variable
+		sawSound = source.GetComponent<AudioClip>();
+		//playerBody = player.GetComponent<Rigidbody2D>();
+		//sawBody = GetComponent<Rigidbody2D>();	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float player_pos = player.transform.position.x;
+
+		if (Mathf.Abs(player_pos - transform.position.x) < triggerDistance) {
+			source.Play();
+		}
+	
+	}
+}
