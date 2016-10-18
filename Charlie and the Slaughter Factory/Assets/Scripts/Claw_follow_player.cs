467c5230efdb12a8cb3c20e19bd0339017b5bd54
idaknow <ides542@aucklanduni.ac.nz>
2016-10-18 18:57:14 +1300

    claw goes up and down and picks up charlie. To fix rotation.

--- Charlie and the Slaughter Factory/Assets/Scripts/Claw_follow_player.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/Claw_follow_player.cs
@@ -1,36 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * This script is the claw following charlie as he runs across the game
+ * 
+ * 
+ * */
+
 public class Claw_follow_player : MonoBehaviour {
 
     public Transform player;
     public float speed = 4.0f;
     private Vector3 newPosition;
-    private bool touchingWall = false;
     private GameObject Claws;
     private float yvalue;
+    private float variable;
+    private bool goDown;
+    private bool hasPlayer = false;
 
     void Start()
     {
+        //finds the y value of the gameobject to use for the claw's position
         Claws = GameObject.FindGameObjectWithTag("Claw");
-        //EdgeCollider2D edge = Claws.GetComponent<EdgeCollider2D>();
         yvalue = Claws.transform.position.y;
+        variable = Random.Range(1f, 5f);
     }
 	
 	void Update () {
-        Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
-        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        // rotate the claw to be facing Charlie
 
         newPosition = transform.position - (transform.right * speed * Time.deltaTime);
-        newPosition.y = yvalue;
-        transform.position = newPosition;
+
+        if (!goDown)
+        {
+                if (transform.position.y >= yvalue)
+                {
+                    newPosition.y = yvalue;
+                }
+                else
+                {
+                    newPosition.y = transform.position.y + (speed * Time.deltaTime);
+                }
+        } else {
+            newPosition.y -= speed * Time.deltaTime;
+        }
+
+        if (hasPlayer)
+        {
+            player.position = newPosition;
+            if (player.position.y == yvalue){
+                hasPlayer = false;
+            }
+        }
+
+        if (Time.time > variable)
+        {
+            if (!goDown)
+            {
+                goDown = (Random.Range(0, 2) == 0);
+            }
+            variable += Random.Range(5f, 10f);
+        }
+
+        if (!hasPlayer && !goDown)
+        {
+            Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+            transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+        }
+
+        transform.position = newPosition; // set claw position as new value
     }
 
-    /*void OnTriggerEnter2D(Collider2D coll)
+    void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "Claw")
+        if (coll.gameObject.tag == "Player")
         {
-            print("touched edge");
+            hasPlayer = true;
         }
-    }*/
+        goDown = false;
+    }
 }
