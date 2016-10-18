5bbfe5b922fe4445e8f84822160f86aaf49868c3
idaknow <ides542@aucklanduni.ac.nz>
2016-10-18 21:12:30 +1300

    Cleaned/ commented code

--- Charlie and the Slaughter Factory/Assets/Scripts/Claw_follow_player.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/Claw_follow_player.cs
@@ -18,7 +18,8 @@ public class Claw_follow_player : MonoBehaviour {
     private float randomVar;
     private bool goDown;
     private bool hasPlayer = false;
-    Quaternion rotation;
+    private Quaternion rotation;
+    private Vector3 position;
 
     void Start()
     {
@@ -26,65 +27,69 @@ public class Claw_follow_player : MonoBehaviour {
         GameObject Claws = GameObject.FindGameObjectWithTag("Claw");
         yvalue = Claws.transform.position.y;
         randomVar = Random.Range(1f, 5f);
-    }
-	
-	void Update () {
-        if (!goDown) {
-                if (transform.position.y >= yvalue)
-                {
-                    newPosition = transform.position - (transform.right * speed * Time.deltaTime);
-                    newPosition.y = yvalue;
-                    rotation = Quaternion.LookRotation(player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
-                    transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-                }
-                else
-                {
-                    newPosition.y = transform.position.y + (speed * Time.deltaTime);
-                Vector3 position = new Vector3();
-                position.x = transform.position.x;
-                position.y = transform.position.y -5 ;
-                rotation = Quaternion.LookRotation(position - transform.position, transform.TransformDirection(Vector3.up));
-                transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-            }
-        } else {
-            newPosition.y -= speed * Time.deltaTime;
-            Vector3 position = new Vector3();
-            position.x = transform.position.x;
-            position.y = transform.position.y - 5;
-            rotation = Quaternion.LookRotation(position - transform.position, transform.TransformDirection(Vector3.up));
-            transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
-        }
 
+        // This is so the claw faces down when it goes down/ up
+        position = new Vector3();
+        position.x = transform.position.x;
+        position.y = transform.position.y - 5;
+
+        // initialises first new position values
+        newPosition = transform.position - (transform.right * speed * Time.deltaTime);
+        newPosition.y = yvalue;
+    }
+
+	void Update () {
+        //checks if claw is going down
+        if (!goDown) {
+                if (transform.position.y >= yvalue) // checks it goes to max the yvalue
+                {
+                    newPosition -= (transform.right * speed * Time.deltaTime); //decrease y position
+                    newPosition.y = yvalue; // makes max yvalue
+                    rotation = Quaternion.LookRotation(player.transform.position - transform.position, transform.TransformDirection(Vector3.up)); // looks at the player
+                }
+                else // makes go up
+                {
+                    newPosition.y += (speed * Time.deltaTime); // increase y position
+                    rotation = Quaternion.LookRotation(position - transform.position, transform.TransformDirection(Vector3.up)); // looks directly down
+                }
+        } else { // makes go down
+            newPosition.y -= speed * Time.deltaTime; //decrease y position
+            rotation = Quaternion.LookRotation(position - transform.position, transform.TransformDirection(Vector3.up)); //looks directly down
+        }
+        // set's the claw's rotation, where it should look
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+
+        //RNG
         if (Time.time > randomVar)
         {
-            if (!goDown)
+            if (!goDown) //if it isn't going down
             {
-                goDown = (Random.Range(0, 2) == 0);
+                goDown = (Random.Range(0, 2) == 0); // 50% chance of true
             }
-            randomVar += Random.Range(2f, 6f);
+            randomVar += Random.Range(2f, 6f); // seconds wait is between 2 and 6
         }
-
-
 
         transform.position = newPosition; // set claw position as new value
 
-        if (hasPlayer)
+        if (hasPlayer) // checks if the claw has Charlie
         {
-            newPosition.y-= 2;
-            player.position = newPosition;
-            if (transform.position.y == yvalue)
+            newPosition.y-= 2; // distance 2 away from the claw
+            player.position = newPosition; // sets this position as the chicken's position
+
+            if (transform.position.y == yvalue) // checks if gets back to the top
             {
-                hasPlayer = false;
+                hasPlayer = false; // sets to false so Chicken drops back down
             }
         }
     }
 
+    //checks if it collides with anything on it's way down
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "Player")
+        if (coll.gameObject.tag == "Player") // sets the boolean as true for when it hits a player
         {
             hasPlayer = true;
         }
-        goDown = false;
+        goDown = false; // sets goDown boolean to false, so can goes back up
     }
 }
