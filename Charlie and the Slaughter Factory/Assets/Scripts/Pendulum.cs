09dad3baf6a8a727ea500274b2495cdea12b414a
Christina <cbel296@aucklanduni.ac.nz>
2016-10-18 19:34:59 +1300

    Created pendulum object and rotates around hinge joint

--- Charlie and the Slaughter Factory/Assets/Scripts/Pendulum.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/Pendulum.cs
@@ -1,63 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Pendulum : MonoBehaviour {
-	public float L = 9.81f;             /*Lenght of the rope*/
-	public float g = 0.81f;             /*Gravity force*/
+public class Pendulum : MonoBehaviour
+{
+    #region Public Variables
+    public Rigidbody2D body2d;
+    public float leftPushRange;
+    public float rightPushRange;
+    public float velocityThreshold;
+    #endregion //Public Variables
 
-	public float theta0=(1/10)*Mathf.PI;/*Initial angle. Must be different from 0*/
-	public float omega0=0;                /*Initial angular velocity*/
-	public bool _________________;
+    #region Private Variables
 
-	public float theta_k;                /*Theta value in step K*/
-	public float omega_k;                /*Omega value in step K*/
-	public float omega_k1;               /*Omega value in step K+1*/
-	public float theta_k1;               /*Theta value in step K+1*/
-	public Vector3 p, p0;
-	Vector3 v;
-	Rigidbody2D rb2d; 
+    #endregion //Private Variables
 
-	void Awake(){
-		omega_k1 = omega0;
-		theta_k1 = theta0;
-		p0 = transform.position;
-		p0.y += L;
-		rb2d = GetComponent<Rigidbody2D>();
-		rb2d.gravityScale = 0f; 
-	}
+    // (Unity Named Methods)
+    #region Main Methods
+    void Start()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+        body2d.angularVelocity = velocityThreshold;
+    }
+    //Update is called by Unity every frame
+    void Update()
+    {
+        Push();
+    }
+    #endregion //Main Methods
 
-	void FixedUpdate(){
-		EulerCromer ();
-		PolarToCartesian ();
-		RotateWithMotion (transform);
-	}
+    //(Custom Named Methods)
+    #region Utility Methods 
+    public void Push()
+    {
+        if (transform.rotation.z > 0
+            && transform.rotation.z < rightPushRange
+            && (body2d.angularVelocity > 0)
+            && body2d.angularVelocity < velocityThreshold)
+        {
+            body2d.angularVelocity = velocityThreshold;
+        }
+        else if (transform.rotation.z < 0
+            && transform.rotation.z > leftPushRange
+            && (body2d.angularVelocity < 0)
+            && body2d.angularVelocity > velocityThreshold * -1)
+        {
+            body2d.angularVelocity = velocityThreshold * -1;
+        }
 
-	/*Implementation of the Euler-Cromer Method*/
-	void EulerCromer(){
-		omega_k = omega_k1;
-		theta_k = theta_k1;
-		omega_k1 = omega_k - (g/L) * theta_k * Time.deltaTime;
-		theta_k1 = theta_k + omega_k1 * Time.deltaTime; 
-	}
+    }
+    #endregion //Utility Methods
 
-	/*Convert Polar coordinates to Cartesian coordinates*/
-	void PolarToCartesian(){
-		p.z = p0.z + L * Mathf.Sin (theta_k1);
-		p.y = p0.y + -L * Mathf.Cos (theta_k1);
-		p.x = p0.x;
-		transform.position = p;
-	}
+    //Coroutines run parallel to other fucntions
+    #region Coroutines
 
-	float radiansToDegrees(float radians){
-		return radians * 180 / Mathf.PI;
-	}
-
-	/*Rotate the rope? and the transform with the motion of the pendulum*/
-	void RotateWithMotion(Transform t){
-		Vector3 rot = t.rotation.eulerAngles;
-		rot.x = radiansToDegrees(-theta_k1);
-		t.rotation = Quaternion.Euler (rot);
-	}
-
-
+    #endregion //Coroutines
 }
