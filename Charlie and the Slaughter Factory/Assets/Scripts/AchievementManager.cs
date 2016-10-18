a0759ebc442db640fe849f810cf703584f8188fb
Abby Thompson <abbyythompson@gmail.com>
2016-10-19 11:24:54 +1300

    the startings of the acheivements

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/AchievementManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AchievementManager : MonoBehaviour {
+
+	//Reference to the prefab of our achievmenet 
+	public GameObject achievementPrefab;
+
+	// Use this for initialization
+	void Start () {
+		CreateAchievement("General");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CreateAchievement(string category){
+		GameObject achievement = GameObject.Instantiate(achievementPrefab);
+	}
+}
