8fbe4695d2681bb74b6a4bec831caf9dad62ef55
idaknow <ides542@aucklanduni.ac.nz>
2016-10-18 21:34:40 +1300

    Added farmer

---
+++ Charlie and the Slaughter Factory/Assets/Chasing_farmer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chasing_farmer : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            print("Caught");
+        }
+    }
+}
