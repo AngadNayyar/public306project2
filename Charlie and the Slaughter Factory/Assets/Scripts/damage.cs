780f98d00c099921e1be77d7d4553c175e5a993f
idaknow <ides542@aucklanduni.ac.nz>
2016-10-19 11:16:48 +1300

    Commented code for damage

--- Charlie and the Slaughter Factory/Assets/Scripts/damage.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/damage.cs
@@ -1,24 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+/**
+ * This script is the script used to add damage to charlie on a collision
+ * */
 
-public class mincer_kill : MonoBehaviour
+public class damage : MonoBehaviour
 {
     public int attackDamage = 100;  //Damage taken due to hit
     GameObject player;  // Reference to Charlie
     PlayerHealth playerHealth;  // Reference to the Charlie's health.
 
-    // Use this for initialization
     void Awake()
     {
+        //instantiate player and player health
         player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = player.GetComponent<PlayerHealth>();
     }
 
-
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject == player)
+        if (col.gameObject == player) // check if collided against player
         {
+            // take health from Charlie
             playerHealth.TakeDamage(attackDamage);
         }
     }
