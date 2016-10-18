80c4bb2c94636e08fb31836077b7cb3b45f7dfdf
brittanywalker <brittwalker@outlook.com>
2016-10-18 22:59:33 +1300

    Random collectible generation fixed!
    Currently capped at 8, need to figure out how to make this adjustable in
    the inspector

--- Charlie and the Slaughter Factory/Assets/Scripts/CollectableSpawn.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/CollectableSpawn.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CollectableSpawn : MonoBehaviour
 {
 	//This array is all of the spawn points and gets added in from the inspector.
-	public Transform[] collectibleSpawns;
+	public List<Transform> collectibleSpawns = new List<Transform>();
     public GameObject collectible;
 
-	private int fixedCollectibleNumber = 10; //This means that there will always be exactly 10 collectible spawns per level.
+	private int fixedCollectibleNumber = 8; //This means that there will always be exactly 10 collectible spawns per level.
 	private int collectibleCount = 0; //Keeps track of how many collectibles have already been spawned.
 
     // Call spawn method to spawn a chicken
@@ -23,13 +25,14 @@ public class CollectableSpawn : MonoBehaviour
 
 			//Loop through each spawn point and randomly generate a number which will decide whether a collectible
 			//will be spawned or not.
-			for (int i = 0; i < collectibleSpawns.Length; i++)
+			for (int i = collectibleSpawns.Count-1; i >= 0; i--)
 			{
 				//If the random number is greater than zero, instatiante the chicken.
-				int collectibleFlip = Random.Range(0, 2);
+				int collectibleFlip = UnityEngine.Random.Range(0, 2);
 				if ((collectibleFlip > 0)&&(collectibleCount < fixedCollectibleNumber)) {
 					Instantiate (collectible, collectibleSpawns [i].position, Quaternion.identity);
 					collectibleCount = collectibleCount + 1; 
+					collectibleSpawns.RemoveAt (i);
 				}
 			}
 		}
