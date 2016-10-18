76e9e9f0cf57c78c6237ac8bae25744389779ad2
brittanywalker <brittwalker@outlook.com>
2016-10-18 18:58:15 +1300

    Fixed number of collectibles generated
    Random generation fixed so that there are a fixed number of collectibles
    per level

--- Charlie and the Slaughter Factory/Assets/Scripts/CollectableSpawn.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/CollectableSpawn.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class CollectableSpawn : MonoBehaviour
 {
-
-    public Transform[] collectibleSpawns;
+	//This array is all of the spawn points and gets added in from the inspector.
+	public Transform[] collectibleSpawns;
     public GameObject collectible;
+
+	private int fixedCollectibleNumber = 10; //This means that there will always be exactly 10 collectible spawns per level.
+	private int collectibleCount = 0; //Keeps track of how many collectibles have already been spawned.
 
     // Call spawn method to spawn a chicken
     void Start()
@@ -15,13 +18,20 @@ public class CollectableSpawn : MonoBehaviour
 
     void Spawn()
     {
-        // for every chicken we want to instantiate
-        for (int i = 0; i < collectibleSpawns.Length; i++)
-        {
-            //if the random number is greater than zero, instatiante the chicken
-            int collectibleFlip = Random.Range(0, 2);
-            if (collectibleFlip > 0)
-                Instantiate(collectible, collectibleSpawns[i].position, Quaternion.identity);
-        }
+        
+		while (collectibleCount < fixedCollectibleNumber) {
+
+			//Loop through each spawn point and randomly generate a number which will decide whether a collectible
+			//will be spawned or not.
+			for (int i = 0; i < collectibleSpawns.Length; i++)
+			{
+				//If the random number is greater than zero, instatiante the chicken.
+				int collectibleFlip = Random.Range(0, 2);
+				if (collectibleFlip > 0) {
+					Instantiate (collectible, collectibleSpawns [i].position, Quaternion.identity);
+					collectibleCount = collectibleCount + 1; 
+				}
+			}
+		}
     }
 }
