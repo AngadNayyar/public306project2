8e03f28662f70bf2253b8685d47212343962eb54
idaknow <ides542@aucklanduni.ac.nz>
2016-10-19 16:30:42 +1300

    Revert "deleted unused script"
    This reverts commit 5ef046c0c692adabdb1f9fc344fe01fe804497d9.

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/Claw_lookat_player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Claw_lookat_player : MonoBehaviour {
+
+    public Transform target;
+
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+	}
+}
