39f65ccb52d4598a60f27bc40fe1aa9564819bf9
idaknow <ides542@aucklanduni.ac.nz>
2016-10-19 16:38:03 +1300

    Revert "Commented code for damage"
    This reverts commit 780f98d00c099921e1be77d7d4553c175e5a993f.

--- Charlie and the Slaughter Factory/Assets/damage.cs
+++ Charlie and the Slaughter Factory/Assets/damage.cs
@@ -1,27 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
-/**
- * This script is the script used to add damage to charlie on a collision
- * */
 
-public class damage : MonoBehaviour
+public class mincer_kill : MonoBehaviour
 {
     public int attackDamage = 100;  //Damage taken due to hit
     GameObject player;  // Reference to Charlie
     PlayerHealth playerHealth;  // Reference to the Charlie's health.
 
+    // Use this for initialization
     void Awake()
     {
-        //instantiate player and player health
         player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = player.GetComponent<PlayerHealth>();
     }
 
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject == player) // check if collided against player
+        if (col.gameObject == player)
         {
-            // take health from Charlie
             playerHealth.TakeDamage(attackDamage);
         }
     }
