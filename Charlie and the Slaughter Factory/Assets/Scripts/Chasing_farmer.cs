f71d3744b492144d4e8e2085cb4e4e79627ee293
idaknow <ides542@aucklanduni.ac.nz>
2016-10-20 00:20:05 +1300

    Added farmer and dog to level 9

---
+++ Charlie and the Slaughter Factory/Assets/Scripts/Chasing_farmer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chasing_farmer : MonoBehaviour
+{
+
+    public float speed = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+}
