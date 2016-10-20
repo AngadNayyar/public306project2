961223c208227dacb30ee77b87639a9180da1072
brittanywalker <brittwalker@outlook.com>
2016-10-21 04:36:35 +1300

    updated saw prefab

--- Charlie and the Slaughter Factory/Assets/Scripts/Audio.cs
+++ Charlie and the Slaughter Factory/Assets/Scripts/Audio.cs
@@ -7,16 +7,21 @@ using System.Collections;
 public class Audio : MonoBehaviour {
 
 	GameObject player;  // Reference to Charlie
-
+	public AudioClip normal;
+	public AudioClip xmas;
+	public AudioClip space;
 	public int triggerDistance; //Distance that Charlie will be from the saw when the sound is played.
+
 	private AudioSource source; //The audio source for the saw object.
+	private GameController gameController;
 
 	void Awake () {
 
 		player = GameObject.FindGameObjectWithTag("Player"); //Sets Charlie to the player variable
 		source = GetComponent<AudioSource> (); //Sets the saw audio source to the source variable
+		gameController = GameObject.Find("GameController").GetComponent<GameController>();
 	}
-	
+
 	//The Update checks Charlie's distance from the saw, and if he is within the trigger distance specified and 
 	//the source isn't already playing, then the AudioClip will be played.
 	void Update () {
@@ -25,7 +30,12 @@ public class Audio : MonoBehaviour {
 		float player_pos = player.transform.position.x;
 
 		if ((Mathf.Abs(player_pos - transform.position.x) < triggerDistance) && (!source.isPlaying)) {
-			source.Play();
+			print(gameController.getTheme());
+			if (gameController.getTheme () == "Normal") {
+				source.clip = normal;
+				source.Play();
+			}
+
 		}
 	
 	}
